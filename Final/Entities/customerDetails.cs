82073a5eca634b226b23b4b74e2ab77fcb5b4f86
Varun Ramesh <68066637+varun-r-boop@users.noreply.github.com>
2022-12-08 14:31:59 +0530

    Added CustomerDetails and CustomerOrders Entities

--- Final/Entities/customerDetails.cs
+++ Final/Entities/customerDetails.cs
@@ -1,6 +1,24 @@
-﻿using System;
+﻿using Final.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 public class customerDetails
 {
-    
+    [Key]
+    [Required]
+    public Guid customerId  { get; set; }
+    [Required]
+    public string customerName { get; set; }
+    [Required]
+    public string customerEmail { get; set; }
+    [Required]
+    public DateOnly customerDob { get; set; }
+    [Required]
+    public string customerPassword { get; set; }
+    [Required]
+    public string customerMobile { get; set; }
+    [Required]
+    public string customerAddress { get; set; }
+
+    public customerOrders customerOrders { get; set; }
 }
---
+++ Final/Entities/customerOrders.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final.Entities
+{
+    public class customerOrders
+    {
+        [Key]
+        public int ordersId { get; set; }
+
+        public ICollection<customerDetails> customerDetails { get; set; }
+        
+    }
+}
